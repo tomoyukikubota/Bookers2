948d3f0b227616ebb9a0b98445dce99aac80917a
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-07-07 14:07:38 +0000

    [Start]応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,18 @@ class User < ApplicationRecord
   def following?(user)
     followings_user.include?(user)
   end
+
+  def self.search(search,word)
+      if search == "forword_match"
+        @user = User.where(["name LIKE?", "#{word}%"])
+      elsif search == "backword_match"
+        @user = User.where(["name LIKE?", "%#{word}"])
+      elsif search == "perfect_match"
+        @user = User.where(name: "#{word}")
+      elsif search == "partial_match"
+        @user = User.where(["name LIKE?", "%#{word}%"])
+      else
+        @user = User.all
+      end
+  end
 end
